0d18cbc594511b219308796c3f983db3799c196f
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-21 13:51:45 +0100

    need to load dailyweather object from json file

--- app.js
+++ app.js
@@ -4,33 +4,81 @@ const app = Vue.createApp({
             name: 'cian' ,
             age: 22,
             showResults: false,
-            weatherData: {
-                city: '',
-                day_one: {},
-                day_two: {},
-                day_three: {},
-                day_four: {},
-                day_five: {},
+            city: 'Enter City...',
+            lat: 0,
+            lon: 0,
+            daily_weather: {
+                day_one: {
+                    unix_dt: 10000000000,
+                    formated_date: "1.12.2023",
+                    temp: 23.4,
+                    rainfall_level: 12,
+                    windspeed: 20,
+                    weather_type: "HOT",
+                    wear_mask: true
+                },
+                day_two: {
+                    unix_dt: 20000000000,
+                    formated_date: "2.12.2023",
+                    temp: 23.4,
+                    rainfall_level: 12,
+                    windspeed: 20,
+                    weather_type: "HOT",
+                    wear_mask: true
+                },
+                day_three: {
+                    unix_dt: 30000000000,
+                    formated_date: "3.12.2023",
+                    temp: 23.4,
+                    rainfall_level: 12,
+                    windspeed: 20,
+                    weather_type: "HOT",
+                    wear_mask: true
+                },
+                day_four: {
+                    unix_dt: 40000000000,
+                    formated_date: "4.12.2023",
+                    temp: 23.4,
+                    rainfall_level: 12,
+                    windspeed: 20,
+                    weather_type: "HOT",
+                    wear_mask: true
+                },
+                day_five: {
+                    unix_dt: 50000000000,
+                    formated_date: "5.12.2023",
+                    temp: 23.4,
+                    rainfall_level: 12,
+                    windspeed: 20,
+                    weather_type: "HOT",
+                    wear_mask: true
+                }
             },
+            days: {
+                day_one: {
+                    title: 'Monday'
+                },
+                day_two: {
+                    title: 'Tuesday'
+                },
+                day_three: {
+                    title: 'Wednesday'
+                }
+            }
         }
     },
 
     methods: {
-        changeTitle(title){
-            console.log("change title pressed")
-            this.name = title
-        },
-
         getWeather(city){
             console.log("Getting Weather Data for ", city)
             fetch('sample_data.json')
                 .then(response => response.json())
                 .then(data => {
-                 // 'data' now contains the JSON object
-                 this.weatherData = data;
-                 this.weatherData.city = city;
-                 console.log(data);
-                })
+                    // 'data' now contains the JSON object
+                    //this.weatherData = data.daily_weather;
+                    this.city = city;
+                    console.log("Set city data field")
+                    console.log(data);})
                 .catch(error => console.error('Error:', error)
             );
 
