3882a7614511685f6b9ae5e0a0062c31cbe2f3c5
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-21 11:08:40 +0100

    search box working

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const app = Vue.createApp({
     data(){
-        
         return {
             name: 'cian' ,
             age: 22,
-            showResults: true,
+            showResults: false,
             weatherData : {}
         }
     },
@@ -16,31 +15,20 @@ const app = Vue.createApp({
         },
 
         getWeather(city){
-            const apiURL = api + city;
-            console.log("Calling API with City: " + city);
-            console.log("CALLING THE URL: " + apiURL);
-
-            fetch(apiURL)
-                .then((res) => {
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return res.json();
+            console.log("Getting Weather Data for ", city)
+            fetch('sample_data.json')
+                .then(response => response.json())
+                .then(data => {
+                 // 'data' now contains the JSON object
+                 this.weatherData = data;
+                 this.weatherData.city = city;
+                 console.log(data);
                 })
-                .then((result) => {
-                // Handle the API response here
-                console.log(JSON.stringify(result, null, 3));
-                // Update your component state or do something with the response data
-                this.weatherData = result; // Assuming you have a data property named 'weatherData'
-                })
-                .catch((error) => {
-                console.error('Error fetching data:', error);
-                // Handle errors (e.g., display an error message to the user)
-                this.errorMessage = 'Error fetching data. Please try again.'; // Assuming you have an error message data property
-                });
+                .catch(error => console.error('Error:', error)
+            );
 
         },
-
+        
         toggleShowResults(){
             this.showResults = !this.showResults
             console.log("show results has been pressed")
